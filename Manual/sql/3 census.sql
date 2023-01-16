2271acab8150e6908670306715503ad42ce72c6a
lufe023 <lufe023@gmail.com>
2023-01-16 15:52:54 -0400

    agregue una validacion de super administrador a parte de administrador para ciertas zonas de la app

--- Manual/sql/3 census.sql
+++ Manual/sql/3 census.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.census
+INSERT INTO census
 (id, first_name, last_name, nickname, citizen_id, age, gender, picture, province, municipality, district, neighbourhood, adress, celphone, telephone, other_phone, leader, "createdAt", "updatedAt")
 VALUES('7414089e-a2ee-47de-80ac-6bdac5f8d4f9'::uuid, 'Luis Fernando', 'Gomez Mateo', 'Emerson', '11300037618', 32, 'Male', '11300037618.png', 1, 2, 3, 4, 'C/ Alberto Perdonmo Edif. 3 Apt B #2', '829-685-8596', NULL, NULL, NULL, '2023-01-15 22:40:42.000', '2023-01-15 22:40:42.000');
--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -3,15 +3,32 @@ const adminValidate = (req, res, next) => {
     const role = req.user.role
 
     //aqui se configura el nivel de los roles de los usuario para permitir entrar o no a distintos lugares
-    if(role === 2 || role === 3){
+    //role = 1 es colaborador
+    // role = 2 es admin
+    //role = 3 es super admin
+    if(role >= 2){
         next()
     }else {
         res.status(401).json({
             message: 'Access Denied!',
-            reason: 'you do not have the required access level'
+            reason: 'you do not have the required access level, require level 2'
         })
     }
+    
 }
 
 
-module.exports = adminValidate
+const superAdminValidate = (req, res, next) => {
+    const role = req.user.role
+
+    //aqui se configura el nivel de los roles de los usuario para permitir entrar o no a distintos lugares
+    if(role === 3){
+        next()
+    }else {
+        res.status(401).json({
+            message: 'Access Denied!',
+            reason: 'you do not have the required access level, require level 3'
+        })
+    }
+}
+module.exports = adminValidate, superAdminValidate
