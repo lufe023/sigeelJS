3cf27395667207da07ce2adb8df81c2312ee3658
lufe023 <lufe023@gmail.com>
2023-08-21 21:35:11 -0400

    arreglando las encuestas tenian la ruta mal

--- src/polls/polls.router.js
+++ src/polls/polls.router.js
@@ -9,12 +9,12 @@ require('../middlewares/auth.middleware')(passport)
     router.route('/')
     .get(passport.authenticate('jwt', {session: false}), pollsServices.getAllPolls)
 
-    router.route('/:userId')
+    router.route('/:id')
     //obtener una encuesta de por id, esta encuesta responde a un elector y trae con ella todo lo relacionado a los candidatos que este puede seleccionar y sus respuesta de modo que se consiga no cometer errores al momento de seleccionar un candidato que no pertenezca a la demarcacion de ese lector
     .get(passport.authenticate('jwt', {session: false}), pollsServices.getPollById)
     
     //actualizacion de los datos de una ecuesta por id que pertenece a un elector, se ha decidido usar patch y no put, puesto que nos interesa poder cambiar de manera parcial los datos obtenidos del elector, esto responde a varias situaciones que se pueden presentar 
-    router.route('/:userId')
+    router.route('/:id')
     .patch(passport.authenticate('jwt', {session: false}), pollsServices.updatePollService)
 
 
