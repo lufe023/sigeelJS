7356d605dbf3781c75cfe9aac35b1492fd81a743
Luis Fernando <lufe023@gmail.com>
2023-11-15 12:49:47 -0400

    ya estamos listos para la primera presentacion

--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -20,6 +20,40 @@ const adminValidate = (req, res, next) => {
     }
 }
 
+const isDelegate = async (req, res, next) => {
+    const user = req.user.id;
+
+    //List of users who have permission to this area
+    const permissionList = [
+        {id: 2, roleName: 'Administrador'},
+        {id: 3, roleName: 'Delegado'},
+        {id: 5, roleName: 'super admin'}
+    ];
+
+    try {
+        const consulta = await getUserById(user);
+        const userRoleId = consulta.user_role.id;
+
+        const tienePermiso = permissionList.some(item => item.id === userRoleId);
+
+        if (tienePermiso) {
+            next();
+        } else {
+            res.status(401).json({
+                message: 'Acceso denegado, necesitas ser Delegado para realizar esta acción!',
+                reason: 'No tienes los permisos necesarios',
+                youRoleIs: consulta.user_role
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({
+            message: 'Access Denied!',
+            reason: 'Error occurred while checking permissions',
+        });
+    }
+};
+
 const isAdministrator = async (req, res, next) => {
     const user = req.user.id;
 
@@ -108,4 +142,4 @@ const superAdminValidate = (req, res, next) => {
 
 
 module.exports = {
-    leaderValidate, adminValidate, itSupportValidate, superAdminValidate, isAdministrator}
+    leaderValidate, adminValidate, itSupportValidate, superAdminValidate, isAdministrator,isDelegate}
--- src/suffrage/suffrage.router.js
+++ src/suffrage/suffrage.router.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const suffrageServices = require('./suffrage.services')
 const passport = require('passport')
-const {leaderValidate, adminValidate, itSupportValidate, superAdminValidate } = require('../middlewares/role.middleware')
+const {leaderValidate, adminValidate, itSupportValidate, superAdminValidate, isDelegate } = require('../middlewares/role.middleware')
 
 
 //get people who voted by college
 router.get('/whovote', passport.authenticate('jwt', {session: false}), suffrageServices.getPeopleWhoVotedServices)
 
 //get people who voted by college
-router.post('/registervote', passport.authenticate('jwt', {session: false}), suffrageServices.createOrUpdateSuffrageServices)
+router.post('/registervote', passport.authenticate('jwt', {session: false}), isDelegate, suffrageServices.createOrUpdateSuffrageServices)
 
 module.exports = router
