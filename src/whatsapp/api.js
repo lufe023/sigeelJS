191d155699cd0d51b258918c316b94d229a15fdd
Luis Fernando <lufe023@gmail.com>
2024-01-31 00:33:41 -0400

    agregando bot de whatsapp con mi foto

---
+++ src/whatsapp/api.js
@@ -0,0 +1,25 @@
+const {Client, LocalAuth} = require("whatsapp-web.js")
+const qrcode = require('qrcode-terminal')
+
+async function startAPI() {
+    const client = new Client({
+        authStrategy: new LocalAuth()
+    })
+
+    client.on('qr', (qr)=> {
+        qrcode.generate(qr, {small: true})
+        console.log('Generando QR')
+    })
+
+    client.on('ready', ()=> {
+        console.log('Client is Ready!')
+    })
+
+    
+
+    await client.initialize()
+
+    return client
+}
+
+module.exports = startAPI
