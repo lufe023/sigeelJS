cc9cddd347fb8e49a28720ca602000c0b1ceca1a
Luis Fernando Gómez Mateo <lufe023@gmail.com>
2025-08-22 22:10:39 -0400

    configurando

--- src/models/ballot.models.js
+++ src/models/ballot.models.js
@@ -1,62 +1,61 @@
 const db = require("../utils/database");
 
-
 const { DataTypes } = require("sequelize");
 const Maps = require("./maps.models");
 const Parties = require("./parties.models");
 
 const Ballot = db.define("ballot", {
-  candidateId: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    allowNull: false,
-    field: 'candidate_id'
-  },
-  name:{
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  party: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    references:{
-      key: 'id',
-      model: Parties
+    candidateId: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        field: "candidate_id",
     },
-  },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    party: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            key: "id",
+            model: Parties,
+        },
+    },
 
-  nomination: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  picture:{
-    type: DataTypes.TEXT,
-    allowNull:true
-  },
-  distritoMunicipal:{
-    type: DataTypes.INTEGER,
-    allowNull:true,
-    field: "distrito_municipal",
-    references:{
-      key: 'id',
-      model: Maps
+    nomination: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-  },
-  municipio:{
-    type: DataTypes.INTEGER,
-    allowNull:true,
-    references:{
-      key: 'id',
-      model: Maps
+    picture: {
+        type: DataTypes.TEXT,
+        allowNull: true,
     },
-  },
-  provincia:{
-    type: DataTypes.INTEGER,
-    references:{
-      key: 'id',
-      model: Maps
+    distritoMunicipal: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: "distrito_municipal",
+        references: {
+            key: "id",
+            model: Maps,
+        },
     },
-  }
+    municipio: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            key: "id",
+            model: Maps,
+        },
+    },
+    provincia: {
+        type: DataTypes.INTEGER,
+        references: {
+            key: "id",
+            model: Maps,
+        },
+    },
 });
 
-module.exports = Ballot
+module.exports = Ballot;
